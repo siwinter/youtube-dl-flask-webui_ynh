f46574c972b511b5e5edc00f49ccae39acb9aeec
thomas blarre <thomas@blarre.net>
2020-02-03 20:21:49 +0100

    use proper class definition

--- conf/ynh_utils.py
+++ conf/ynh_utils.py
@@ -1,15 +1,15 @@
 class PrefixMiddleware(object):
-#class for URL sorting 
+
     def __init__(self, app, prefix=''):
         self.app = app
         self.prefix = prefix
 
     def __call__(self, environ, start_response):
-        #in this line I'm doing a replace of the word flaskredirect which is my app name in IIS to ensure proper URL redirect
-        if environ['PATH_INFO'].lower().replace('/flaskredirect','').startswith(self.prefix):
-            environ['PATH_INFO'] = environ['PATH_INFO'].lower().replace('/flaskredirect','')[len(self.prefix):]
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
             environ['SCRIPT_NAME'] = self.prefix
             return self.app(environ, start_response)
         else:
-            start_response('404', [('Content-Type', 'text/plain')])            
+            start_response('404', [('Content-Type', 'text/plain')])
             return ["This url does not belong to the app.".encode()]
